8c99d0883baa4d9f4f71a19c51e199592aaf7653
Alexandre MOTTIER <Alexandre.MOTTIER@alticap.com>
2021-02-02 17:57:56 +0100

    Mise en page code

--- index.php
+++ index.php
@@ -17,13 +17,17 @@ de mot de passe</h1>
 <div style="text-align: center;">
 </div>
 
-<div style="text-align: center;"><?php $minuscule = array('alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot', 'golf', 'hotel', 'india', 'juliette', 'kilo', 'lima', 'november', 'oscar', 'papa', 'quebec', 'romeo', 'sierra', 'tango', 'uniform', 'victor', 'whisky', 'yankee', 'zulu');
+<div style="text-align: center;">
+<?php
+$minuscule = array('alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot', 'golf', 'hotel', 'india', 'juliette', 'kilo', 'lima', 'november', 'oscar', 'papa', 'quebec', 'romeo', 'sierra', 'tango', 'uniform', 'victor', 'whisky', 'yankee', 'zulu');
 $majuscule = array('ALPHA', 'BRAVO', 'CHARLIE', 'DELTA', 'ECHO', 'FOXTROT', 'GOLF', 'HOTEL', 'INDIA', 'JULIETTE', 'KILO', 'LIMA', 'NOVEMBER', 'OSCAR', 'PAPA', 'QUEBEC', 'ROMEO', 'SIERRA', 'TANGO', 'UNIFORM', 'VICTOR', 'WHISKY', 'YANKEE', 'ZULU');
 $special = array('@', '+', '-', '&');
 echo $minuscule[array_rand($minuscule)];
 echo $majuscule[array_rand($majuscule)];
 echo $special[array_rand($special)];
-echo(rand(1,99));?><br>
+echo(rand(1,99));
+?>
+<br>
 
 </div>
 <div style="text-align: center;"></div>
